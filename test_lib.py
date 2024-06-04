2c08c79c9a831dca8b6c9491867e2c81f92de108
Mekso <129759696+Rhomekso@users.noreply.github.com>
2024-06-04 12:37:44 +0200

    Create test_lib.py

---
+++ test_lib.py
@@ -0,0 +1,10 @@
+import lib
+
+def test_power():
+    assert lib.getfibonacci(0) == 0
+    assert lib.getfibonacci(1) == 1
+    assert lib.getfibonacci(2) == 1
+    assert lib.getfibonacci(3) == 2
+    assert lib.getfibonacci(4) == 3
+    assert lib.getfibonacci(5) == 5
+    assert lib.getfibonacci(6) == 8
