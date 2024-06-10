5557228747c0fd8c71fcf3c891a4e1619ffb26e2
Mekso <129759696+Rhomekso@users.noreply.github.com>
2024-06-10 20:16:46 +0200

    Created script_yaml.sh

---
+++ script_yaml.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d "/home/Winc_CD_repo" ]; then
+  git clone https://github.com/Rhomekso/Winc-Assignment-CD.git /home/Winc_CD_repo
+  echo "Repository cloned."
+else
+  if [ -d "/home/Winc_CD_repo/.git" ]; then
+    cd /home/Winc_CD_repo
+    git pull
+    echo "Changes pulled from the repository."
+  else
+    rm -rf /home/Winc_CD_repo
+    git clone https://github.com/Rhomekso/Winc-Assignment-CD.git /home/Winc_CD_repo
+    echo "Repository cloned."
+  fi
+fi
